87420de418f211c5a667b17ec3842f40f28bd68e
ichuli <ichulidesigns@gmail.com>
2024-11-06 17:35:17 +0300

    real upload now 😁

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const ps = document.querySelector('#ps')
 const sp = document.querySelector('#sp')
 const rate = document.querySelector('#rate')
 const exact = document.querySelector('#exact')
+const ubp = document.querySelector('#ubp')
+
 
 cp.addEventListener('keyup', ()=>{
-  exact.innerText = (cp.value * rate.value)/ps.value
+  exact.innerText = Math.round((cp.value * rate.value)/ps.value)
+  ubp.value = cp.value / ps.value
   let xed = Math.ceil((cp.value * rate.value)/ps.value)
   sp.value = Math.ceil(xed/100) * 100
 })
 
+
 rate.addEventListener('change', ()=>{
+  ubp.value = cp.value / ps.value
   exact.innerText = (cp.value * rate.value)/ps.value
   let xed = Math.ceil((cp.value * rate.value)/ps.value)
   sp.value = Math.ceil(xed/100) * 100
 })
 
 ps.addEventListener('change', ()=>{
+  ubp.value = cp.value / ps.value
   exact.innerText = (cp.value * rate.value)/ps.value
   let xed = Math.ceil((cp.value * rate.value)/ps.value)
   sp.value = Math.ceil(xed/100) * 100
